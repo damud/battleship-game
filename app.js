94acb2a3688b7bdbe0d0b1c41418c87bedfe8431
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-02-15 19:46:50 +0100

    Create random generate ships on gameboard

--- app.js
+++ app.js
@@ -30,3 +30,43 @@ function createBoard(color, user) {
 }
 createBoard('yellow', 'player');
 createBoard('pink', 'computer');
+
+// Creating Ship 
+class Ship {
+    constructor(name, length) {
+        this.name = name;
+        this.length = length;
+    }
+}
+
+const destroyer = new Ship('destroyer', 2);
+const submarine = new Ship('submarine', 3);
+const cruiser = new Ship('cruiser', 3);
+const battleship = new Ship('battleship', 4);
+const carrier = new Ship('carrier', 5);
+
+const ships = [destroyer, submarine, carrier, battleship, cruiser]
+
+function addShipPiece(ship) {
+    const allBoardBlocks = document.querySelectorAll('#computer div');
+    let randomBoolean = Math.random() < 0.5;
+    let isHorizontal = randomBoolean;
+    let randomStartIndex = Math.floor(Math.random() * width * width);
+    console.log(randomStartIndex)
+
+    let shipBlocks = [];
+
+    for (let i = 0; i < ship.length; i++) {
+        if (isHorizontal) {
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+        } else {
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width]);
+        }
+    }
+      console.log(shipBlocks);
+      shipBlocks.forEach(shipBlock => {
+        shipBlock.classList.add(ship.name);
+        shipBlock.classList.add('taken');
+      })
+}
+ships.forEach(ship => addShipPiece(ship));
