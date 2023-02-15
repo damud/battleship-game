e9fc0bce926a3585cdac358715fc9980c0d2acfc
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-02-15 18:15:21 +0100

    Create 100 block space to put on ships in them

--- app.js
+++ app.js
@@ -11,17 +11,22 @@ function flip() {
 }
 
 // Creating Boards
+flipButton.addEventListener('click', flip);
 
 const width = 10;
 
-function createBoard(color) {
+function createBoard(color, user) {
     const gameBoardContainer = document.createElement('div');
     gameBoardContainer.classList.add('game-board');
     gameBoardContainer.style.backgroundColor = color;
-
+    gameBoardContainer.id = user;
+    for (let i =0; i < width * width; i++) {
+       const block =  document.createElement('div');
+       block.classList.add('block');
+       block.id = i;
+       gameBoardContainer.append(block);
+    }
     gamesBoardContainer.append(gameBoardContainer);
 }
-createBoard('yellow');
-createBoard('pink');
-
-flipButton.addEventListener('click', flip);
+createBoard('yellow', 'player');
+createBoard('pink', 'computer');
