e411627c6fe6432ed1d1b8bab74a6f2eb900d165
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-02-15 18:01:44 +0100

    Create ships and gamesboards

---
+++ app.js
@@ -0,0 +1,27 @@
+const gamesBoardContainer = document.querySelector('#gamesboard-container');
+const optionContainer = document.querySelector('.option-container');
+const flipButton = document.querySelector('#flip-button');
+
+
+let angle = 0;
+function flip() {
+    const optionShips = Array.from(optionContainer.children)
+    angle = angle === 0 ? 90 : 0;
+    optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`);
+}
+
+// Creating Boards
+
+const width = 10;
+
+function createBoard(color) {
+    const gameBoardContainer = document.createElement('div');
+    gameBoardContainer.classList.add('game-board');
+    gameBoardContainer.style.backgroundColor = color;
+
+    gamesBoardContainer.append(gameBoardContainer);
+}
+createBoard('yellow');
+createBoard('pink');
+
+flipButton.addEventListener('click', flip);
