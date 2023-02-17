e6711c3bf49a7a7e96e0502cda56c3c0c09df58e
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-02-17 20:04:17 +0100

    Check if player hit computer ship or not

--- app.js
+++ app.js
@@ -166,11 +166,20 @@ function startGame () {
     }
 }
 
+let playerHits = [];
+let computerHits = [];
+
 function handleClick(e) {
    if (!gameOver) {
       if (e.target.classList.contains('taken')) {
         e.target.classList.add('boom');
         infoDisplay.textContent = 'You hit the computers ship!';
+        let classes = Array.from(e.target.classList);
+        classes = classes.filter(className => className !== 'block');
+        classes = classes.filter(className => className !== 'boom');
+        classes = classes.filter(className => className !== 'taken');
+        playerHits.push(...classes);
+        console.log(playerHits);
       }
    } 
 }
